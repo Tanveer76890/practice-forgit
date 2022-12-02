f47cef0acff7ac3975e515f7a6984edffb23b89a
Muhammad Tanveer <mtanveer76890@gmail.com>
2022-12-02 19:33:14 +0500

    code4 file ma divide  method added

--- code4.js
+++ code4.js
@@ -3,3 +3,8 @@ let x=40;
 let y = 60;
 const mul = x * y;
 console.log("multiple of two number is : " + mul);
+// add division method 
+let i = 200;
+let j= 2;
+let div = i/j;
+console.log("divide of two  number are :" + div);
