e9871af92d3480e3bd65233fd932e42f2757deb2
Muhammad Tanveer <mtanveer76890@gmail.com>
2022-12-02 19:30:17 +0500

    code3 file ma sum method added

--- code3.js
+++ code3.js
@@ -2,3 +2,8 @@ let a = 10;
 const b = 20;
 let sub = a-b;
 console.log("Subtract of two number are : " + sub);
+// add concept 
+let c = 100;
+let d = 200;
+const sum = c +d;
+console.log("sum of two number are : " + sum);
